2cb85ca916b5f912a6df5249b5d08d5e48b64b0e
arthurshoppy <max.david1007@gmail.com>
2023-10-12 02:32:09 +0200

    setup supabase functions

---
+++ backend/supabase/functions/createUser/index.ts
@@ -0,0 +1,57 @@
+import { serve } from 'https://deno.land/std@0.177.0/http/server.ts'
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.38.0"
+
+import { corsHeaders } from '../_shared/cors.ts'
+
+serve(async (req: Request) => {
+  // This is needed if you're planning to invoke your function from a browser.
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
+  }
+
+  try {
+    // Create a Supabase client with the Auth context of the logged in user.
+    const userClient = createClient(
+      // Supabase API URL - env var exported by default.
+      Deno.env.get('SUPABASE_URL') ?? '',
+      // Supabase API ANON KEY - env var exported by default.
+      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+      // Create client with Auth context of the user that called the function.
+      // This way your row-level-security (RLS) policies are applied.
+      { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+    )
+
+    const adminClient = createClient(
+      Deno.env.get('SUPABASE_URL') ?? '',
+      Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
+    )
+
+    // Now we can get the session or user object
+    const {
+      data: { user },
+    } = await userClient.auth.getUser()
+
+    // And we can run queries in the context of our authenticated user
+    const { data, error } = await adminClient.from('users').select('*')
+    if (error) throw error
+
+    return new Response(JSON.stringify({ user, data }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 200,
+    })
+  } catch (err) {
+    setTimeout(() => {
+      throw err
+    }, 1)
+    return new Response(JSON.stringify({ error: err.message }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
+})
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
