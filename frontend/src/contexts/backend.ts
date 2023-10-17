ffb66e08b6a597f1832b499cb86e1cd0884037b1
Fabbaist <weberfabian1@gmx.de>
2023-10-17 16:34:24 +0200

    building transactions with cometh

--- frontend/src/contexts/backend.ts
+++ frontend/src/contexts/backend.ts
@@ -1,4 +1,5 @@
 import {
+  ComethProvider,
   ComethWallet,
   ConnectAdaptor,
   SupportedNetworks
@@ -9,6 +10,9 @@ export function createFauxBackendCtx() {
 
   let walletAdaptor: ConnectAdaptor;
   let walletInstance: ComethWallet;
+  let sDAIMainnetContract = '0x83F20F44975D03b1b09e64809B757c47f942BEeA';
+  let sDAIChiadoContract = '0x20e5eB701E8d711D419D444814308f8c2243461F';
+  let provider: ComethProvider;
 
   const ctx = {
     
@@ -23,29 +27,68 @@ export function createFauxBackendCtx() {
 				authAdapter: walletAdaptor,
 				apiKey: import.meta.env.VITE_COMMETH,
 			});
+      
 		},
 
-    //   async initializeAccount() {
 
-    //     await walletInstance.connect(localStorageAddress);
+    //   async initializeAccount() {
+    //    await walletInstance.connect(localStorageAddress);
     //   },
 
-    //   async stakeSDAI() {
+      async stakeSDAI(amount: number, ) {
+        const value = amount;
+        provider = new ComethProvider(walletInstance);
+       
+        const txValues =  {
+          to: sDAIMainnetContract, //deposit xDAI for sDAI
+          value: "0x00", //User Input
+          data: "0x", // Calldata
+        };
+        
+        const tx = await walletInstance.sendTransaction(txValues);
+        const txPending = await provider.getTransaction(tx.safeTxHash);
+        await txPending.wait();
+    },
 
-    //   }
-    async makeSwapAndDeposit() {
-      const txValues = [{
-        to: targetAddress2, //make a swap to DAI
+    async testTransaction() {
+      const txValues =  {
+        to: sDAIMainnetContract, //deposit xDAI for sDAI
         value: "0x00", //User Input
-        data: "0x",
+        data: "0x", // Calldata
+      };
+      
+      const tx = await walletInstance.sendTransaction(txValues);
+      const txPending = await provider.getTransaction(tx.safeTxHash);
+      await txPending.wait();
+    },
+  
+    async unstakeSDAI() {
+      const txValues =  {
+        to: sDAIMainnetContract, //withdraw xDAI for sDAI
+        value: "0x00", //User Input
+        data: "0x", // Calldata for function unstake/withdraw
+      };
+      
+      const tx = await walletInstance.sendTransaction(txValues);
+      const txPending = await provider.getTransaction(tx.safeTxHash);
+      await txPending.wait();
+  },
+      
+    async makeSwapAndDeposit() {
+      provider = new ComethProvider(walletInstance);
+      const txValues = [{
+        to: targetAddress2, //make a swap to ETH to xDAI(if we build it with EURe)
+        value: "0x00", //User Input
+        data: "0x", //Calldata of the tx
       }, {
-        to: targetAddress2, //deposit DAI for sDAI
+        to: sDAIMainnetContract, //deposit xDAI for sDAI
         value: "0x00", //User Input
         data: "0x", //
       }];
            
-      
-      const txPending = await walletInstance.sendBatchTransactions(txValues);
+
+      const txBatch = await walletInstance.sendBatchTransactions(txValues);
+      const txPending = await provider.getTransaction(txBatch.safeTxHash);
       await txPending.wait();
     },
   
@@ -53,6 +96,7 @@ export function createFauxBackendCtx() {
     //   ctx.createAccount()
     // }
     async createWallet() {
+        
       const localStorageAddress = window.localStorage.getItem("walletAddress");
 
         if (localStorageAddress) {
