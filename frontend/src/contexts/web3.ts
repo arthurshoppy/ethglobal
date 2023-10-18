3bfe32b8397ab0ecd1843aa5d8c94f95bdc37a5f
arthurshoppy <max.david1007@gmail.com>
2023-10-18 02:37:01 +0200

    use on chain state

--- frontend/src/contexts/web3.ts
+++ frontend/src/contexts/web3.ts
@@ -1,45 +1,26 @@
 import { readable, writable } from 'svelte/store';
 import { cachedStore, consistentStore, refreshableStore } from '../helpers/reactivity-helpers';
-import { ethers, providers, Contract, BigNumber, utils } from "ethers"
+import { providers, Contract, BigNumber, utils } from "ethers"
 import { getContext } from 'svelte';
 import type { createFauxBackendCtx } from './backend';
-
-const ADDR = {
-	main: {
-		curveEUR: "0x056c6c5e684cec248635ed86033378cc444459b0",
-		EURe: "0xcB444e90D8198415266c6a2724b7900fb12FC56E",
-		sDAI: "0xaf204776c7245bF4147c2612BF6e5972Ee483701",
-		cUSDC: "0xF25212E676D1F7F89Cd72fFEe66158f541246445",
-		USDC: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"
-	},
-	test: {
-		curveEUR: "",
-		EURe: "0xb106ed7587365a16b6691a3D4B2A734f4E8268a2",
-		sDAI: "0x20e5eB701E8d711D419D444814308f8c2243461F",
-		cUSDC: "0xF09F0369aB0a875254fB565E52226c88f10Bc839",
-		USDC: "0xDB3cB4f2688daAB3BFf59C24cC42D4B6285828e9"
-	}
-}.main
+import { Mainnet } from '../shared/addresses';
 
 export function createWeb3Ctx() {
 
-  const backend =
-		getContext<ReturnType<typeof createFauxBackendCtx>>("fauxBackend");
+	const providerGnosis = new providers.JsonRpcProvider(import.meta.env.VITE_RPC_GNOSIS)
+	const providerPolygon = new providers.JsonRpcProvider(import.meta.env.VITE_RPC_POLYGON)
 
-	let providerGnosis: ethers.providers.JsonRpcProvider
-	let providerPolygon: ethers.providers.JsonRpcProvider
+	const backend =
+		getContext<ReturnType<typeof createFauxBackendCtx>>("fauxBackend");
 
   const ctx = {
 
 		ratioUsdEur: cachedStore(writable<number>(0.95)),
 
 		async updateBalances() {
-			// 0x614bbb4705D1016E6D218322106C9760E920B476 eure/usdc uni
-			// 0x7F62D301b80ea7c09665d104B86C0cf140634801 eure/dai uni
-
-			const euroCurve = new Contract("0x056c6c5e684cec248635ed86033378cc444459b0", ["function last_prices() view returns (uint256)"], providerGnosis)
-			const price: BigNumber = await euroCurve.last_prices()
-			ctx.ratioUsdEur.set(Number(utils.formatUnits(price, 18)))
+			const euroCurve = new Contract(Mainnet.eureusdCurve, ["function price_scale() returns (uint256)"], providerGnosis)
+			const price: BigNumber = await euroCurve.callStatic.price_scale()
+			ctx.ratioUsdEur.set(2 - Number(utils.formatUnits(price, 18)))
 
 			ctx.balances.EURe.refresh()
 			ctx.balances.sDAI.refresh()
@@ -58,25 +39,21 @@ export function createWeb3Ctx() {
 			})),
 	
 			EURe: cachedStore(refreshableStore(writable<number>(0), async () => {
-				// 0xcB444e90D8198415266c6a2724b7900fb12FC56E - gnosis EURe
-				const eure = new Contract("0xb106ed7587365a16b6691a3D4B2A734f4E8268a2", ["function balanceOf(address) view returns (uint256)"], providerGnosis)
-				const balance: BigNumber = await eure.balanceOf(backend.address.current)
+				const eure = new Contract(Mainnet.EURe, ["function balanceOf(address) returns (uint256)"], providerGnosis)
+				const balance: BigNumber = await eure.callStatic.balanceOf(backend.address.current)
 				return Number(utils.formatUnits(balance, 18))
 			})),
 	
 			sDAI: cachedStore(refreshableStore(writable<number>(0), async () => {
-				// 0xaf204776c7245bF4147c2612BF6e5972Ee483701 - gnosis sDAI
-				const sdai = new Contract("0x20e5eB701E8d711D419D444814308f8c2243461F", ["function maxRedeem(address) view returns (uint256)"], providerGnosis)
-				const balance: BigNumber = await sdai.maxRedeem(backend.address.current)
+				const sdai = new Contract(Mainnet.sDAI, ["function maxRedeem(address) returns (uint256)"], providerGnosis)
+				const balance: BigNumber = await sdai.callStatic.maxRedeem(backend.address.current)
 				return Number(utils.formatUnits(balance, 18))
 			})),
 	
 			cUSDC: cachedStore(refreshableStore(writable<number>(0), async () => {
-				// 0xF25212E676D1F7F89Cd72fFEe66158f541246445 polygon cUSDC
-				// 0xDB3cB4f2688daAB3BFf59C24cC42D4B6285828e9 mumbai USDC
-				const cusdc = new Contract("0xF09F0369aB0a875254fB565E52226c88f10Bc839", ["function balanceOf(address) view returns (uint256)"], providerPolygon)
-				const balance: BigNumber = await cusdc.balanceOf(backend.address.current)
-				return Number(utils.formatUnits(balance, 6)) // polygon usdc = 8 decimals, mumbai = 6 decimals
+				const cusdc = new Contract(Mainnet.cUSDC, ["function balanceOf(address) returns (uint256)"], providerPolygon)
+				const balance: BigNumber = await cusdc.callStatic.balanceOf(backend.address.current)
+				return Number(utils.formatUnits(balance, 6))
 			}))
 		}
 
@@ -84,12 +61,6 @@ export function createWeb3Ctx() {
 
 	backend.address.subscribe(addr => {
 		if (!addr) return
-		
-		if (!providerGnosis) {
-			providerGnosis = new providers.JsonRpcProvider(import.meta.env.VITE_RPC_GNOSIS)
-			providerPolygon = new providers.JsonRpcProvider(import.meta.env.VITE_RPC_POLYGON)
-		}
-
 		ctx.updateBalances()
 	})
 
