573eed470297fcf8935648dc22f3dbce6bc19244
arthurshoppy <max.david1007@gmail.com>
2023-10-21 21:05:57 +0200

    add address and public key export

--- frontend/src/routes/Settings.svelte
+++ frontend/src/routes/Settings.svelte
@@ -2,17 +2,52 @@
 	import { getContext } from "svelte";
 	import BackButtton from "../elements/BackButtton.svelte";
 	import type { createFauxBackendCtx } from "../contexts/backend";
+	import Button from "../elements/Button.svelte";
 
 	const backend =
 		getContext<ReturnType<typeof createFauxBackendCtx>>("fauxBackend");
+
+	const address = backend.address;
+
+	let keyRevealed = false;
 </script>
 
-<div class="grid gap-2">
+<div class="grid gap-4">
 	<BackButtton />
 
 	<div class="text-3xl/7">Settings</div>
 
-	<div>
-		{backend.getPrivateKey()}
+	<div class="bg-white rounded-lg p-2 grid gap-2">
+		<div class="flex flex-col">
+			<span class="text-lg">Address</span>
+			<div
+				class="text-sm bg-neutral-100 hover:bg-neutral-200 rounded px-1 py-px"
+			>
+				{$address}
+			</div>
+		</div>
+
+		<div class="flex flex-col">
+			<span class="text-lg">Private Key</span>
+			<div class="text-sm flex gap-2">
+				<div
+					class="break-words max-w-[268px] bg-neutral-100 hover:bg-neutral-200 rounded px-1 py-px"
+				>
+					{keyRevealed
+						? backend.getPrivateKey()
+						: Array(76)
+								.fill(1)
+								.map(() => "*")
+								.reduce((p, c) => p + c, "")}
+				</div>
+				<Button
+					class="!rounded !my-0 text-white grow !px-1"
+					disabled={keyRevealed}
+					on:click={() => (keyRevealed = true)}
+				>
+					Reveal Key
+				</Button>
+			</div>
+		</div>
 	</div>
 </div>
